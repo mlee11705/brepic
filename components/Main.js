62452c3f9e32fd772eb8c8ced5102111f7025da0
eelekim <mlee8343@gmail.com>
2022-04-22 12:26:48 -0700

    globe

--- components/Main.js
+++ components/Main.js
@@ -25,7 +25,8 @@ export default function Main() {
         <video
           class="object-cover object-center w-full mb-10 border-gray-200 dark:border-gray-900 g327 border rounded-lg shadow-md"
           alt="hero"
-          src="./images/83209210-136f-4dfd-aa3e-7e0cf39ade70.webm" type="video/webm"
+          src="./images/zoomed-in-arcs.h264.mp4" 
+          type="video/mp4"
         ></video>
       </div>
       <section class="text-gray-600 body-font">
