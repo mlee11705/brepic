1d6e63ebffa4f078c4002bca03d21b521d0c07d0
eelekim <mlee8343@gmail.com>
2022-04-23 14:05:46 -0700

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -21,9 +21,6 @@ export default function Main() {
             and get access to the world of intro only professionals.
           </p>
         </div>
-        <button class="px-4 py-2 mt-2 text-sm font-semibold text-white bg-blue-600 transition duration-500 ease-in-out transform bg-transparent rounded-lg dark:text-gray-300 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-          Apply
-        </button>
       </div>
       <div class="container flex flex-col items-center justify-center py-8 mx-auto rounded-lg md:p-1 p-3">
         <img
